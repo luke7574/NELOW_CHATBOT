bd668ed9afffc198b6488f92d2622324607766c3
luke7574 <luke7574@naver.com>
2025-04-22 14:29:50 +0900

    wook

--- mistral.py
+++ mistral.py
@@ -63,3 +63,6 @@ def answer_question(question, chunks, index, embedding_model, top_k=3):
 question = "누수음 수집절차에 대해 설명해줘"
 answer = answer_question(question, chunks, index, embedding_model)
 print("▶ 답변:", answer)
+
+
+print(1)
